4638cb444719ee4827f70947703df81f5b33647d
jeonhyeonseon <good_pretty@naver.com>
2025-01-06 22:17:05 +0900

    chore: 의존성 및 Tailwind CSS 설정 파일 업데이트

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Community/html/**/*.html",
+    "./Community/css/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
